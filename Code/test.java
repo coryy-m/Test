ebe46796f8659306e49f71f73e053517765011b2
Cory <marriottc97@gmail.com>
2018-09-28 17:50:46 -0400

    More testing

--- Code/test.java
+++ Code/test.java
@@ -3,8 +3,11 @@
 // Why is this not commiting
 // Hey it finally works
 
-public class test{
-  public static void main(String args[]){
-    System.out.println("Hello World!!")
-  }
+public class test {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
