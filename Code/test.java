9664948f358bb192aa6a6f037e2731726fd7980c
Cory <marriottc97@gmail.com>
2018-09-28 12:59:37 -0400

    Actual code now

--- Code/test.java
+++ Code/test.java
@@ -2,3 +2,9 @@
 // I'm testing an update to the code
 // Why is this not commiting
 // Hey it finally works
+
+public main(){
+  public static void main(String args[]){
+    System.out.println("Hello World!!")
+  }
+}
